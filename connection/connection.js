e240fcc470978ad096948609699847ae780d0941
cschmidt216 <iamdrainy@gmail.com>
2023-05-29 22:44:53 -0700

    Revert "connection update"
    This reverts commit 37bb1221ff15396bc90f0dce349f771cc776618d.

--- connection/connection.js
+++ connection/connection.js
@@ -1,14 +1,21 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+const Sequelize = require('sequelize');
+let sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
-module.exports = sequelize;
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
