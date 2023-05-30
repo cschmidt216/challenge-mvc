37bb1221ff15396bc90f0dce349f771cc776618d
cschmidt216 <iamdrainy@gmail.com>
2023-05-29 19:27:01 -0700

    connection update

--- connection/connection.js
+++ connection/connection.js
@@ -1,21 +1,14 @@
+const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
-let sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+module.exports = sequelize;
