932e3475be6bcb81e4fc0a3e607c873101dac06c
cschmidt216 <iamdrainy@gmail.com>
2023-05-29 22:40:30 -0700

    Revert "style and bugs"
    This reverts commit 36db4b302f773f60454ccf2bca09493f40f3cdb2.

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -7,6 +7,15 @@ function format_date(date) {
     return `${month}/${day}/${year}`;
   }
   
+  function format_plural(word, amount) {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+  
+    return word;
+  }
+  
   module.exports = {
     format_date,
+    format_plural
   };
